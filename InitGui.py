217a54504dba3aeba58c2e528ecab653c4f9da02
easyw <easyw@katamail.com>
2018-05-03 16:22:48 +0200

    better tree icons

--- InitGui.py
+++ InitGui.py
@@ -14,7 +14,7 @@ ksu_wb_version='v 7.7.0'
 global myurlKWB
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=96 #v7.7.0
+mycommitsKWB=97 #v7.7.0
 
 import FreeCAD, FreeCADGui, Part, os, sys
 import re, time
