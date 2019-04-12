86c1fa0f362f75dcd438640324cf7b3998fe8703
easyw <easyw@katamail.com>
2019-04-12 14:40:41 +0200

    minor alignement

--- InitGui.py
+++ InitGui.py
@@ -14,7 +14,7 @@ ksu_wb_version='v 7.9.0'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=249 #v7.9.0
+mycommitsKWB=251 #v7.9.0
 
 import FreeCAD, FreeCADGui, Part, os, sys
 import re, time
