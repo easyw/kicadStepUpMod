8a09af22563e03c1e7a1db0b18f93d4612597635
easyw <easyw@katamail.com>
2019-08-26 12:36:11 +0200

    including missing .ui modification for LinkGroups

--- InitGui.py
+++ InitGui.py
@@ -10,11 +10,11 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely useable           *
 #*                                                                          *
 
-ksu_wb_version='v 9.0.2'
+ksu_wb_version='v 9.0.3'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=280 #v9.0.2
+mycommitsKWB=281 #v9.0.3
 
 import FreeCAD, FreeCADGui, Part, os, sys
 import re, time
