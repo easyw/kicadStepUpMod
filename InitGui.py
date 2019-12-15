e2477765b50043b504d9ea377cf41d03627fc55f
easyw <easyw2g@gmail.com>
2019-12-15 11:30:48 +0100

    aligning commits nbr

--- InitGui.py
+++ InitGui.py
@@ -14,7 +14,7 @@ ksu_wb_version='v 10.0.4'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=313 #v10.0.1
+mycommitsKWB=322 #v10.0.4
 
 import FreeCAD, FreeCADGui, Part, os, sys
 import re, time
