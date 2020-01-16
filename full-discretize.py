0ed6fa07e7cdf1f6c39e9a72b0358e9a0695663f
easyw <easyw2g@gmail.com>
2020-01-16 23:17:53 +0100

    more baby steps

--- full-discretize.py
+++ full-discretize.py
@@ -3,7 +3,7 @@
 #****************************************************************************
 
 import FreeCAD, FreeCADGui
-import sys,os
+import sys,os, math
 import Draft
 import PySide
 from PySide import QtGui, QtCore
@@ -11,6 +11,33 @@ from PySide import QtGui, QtCore
 q_deflection = 0.005 #0.02 ##0.005
 tol = 0.01
 constr = 'coincident' #'all'
+
+def distance(p0, p1):
+    return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
+
+def find_sequence (elist,idx, min_dist):
+    
+        ep0 = elist[idx].Vertexes[0].Point
+        ep1 = elist[idx].Vertexes[1].Point
+        ep2 = elist[idx+1].Vertexes[0].Point
+        ep3 = elist[idx+1].Vertexes[1].Point
+        if distance(ep0,ep2) < min_dist:
+            first_pnt = ep1
+            common_pnt = ep0
+            last_pnt = ep3
+        elif distance(ep0,ep3) < min_dist:
+            first_pnt = ep1
+            common_pnt = ep3
+            last_pnt = ep2
+        elif distance(ep1,ep3) < min_dist:
+            first_pnt = ep0
+            common_pnt = ep1
+            last_pnt = ep2
+        elif distance(ep1,ep2) < min_dist:
+            first_pnt = ep0
+            common_pnt = ep1
+            last_pnt = ep3
+        return first_pnt, common_pnt, last_pnt
 
 sel = FreeCADGui.Selection.getSelection()
 if len(sel) != 1:
@@ -46,34 +73,37 @@ else:
     edge_width = 0.1 
     edges = shp.Edges
     segments_nbr=len(edges)
+    if segments_nbr<3:
+        stop
+    
     fillzone = """  (zone (net 0) (net_name "") (layer B.Cu) (tstamp 0) (hatch edge 0.508)"""+os.linesep
     fillzone+="""    (connect_pads (clearance 0.508))"""+os.linesep
     fillzone+="""    (min_thickness 0.254)"""+os.linesep
     fillzone+="""    (fill yes (arc_segments 32) (thermal_gap 0.508) (thermal_bridge_width 0.508))"""+os.linesep
     fillzone+="""    (polygon"""+os.linesep
-    i=1
+    i=0
     pts = "      (pts "+os.linesep
-    for e in edges:
-        if 'Line' not in e.Curve.TypeId:
+    first_pnt, common_pnt, last_pnt = find_sequence (edges,i,tol)
+    i+=1
+    for e in edges[1:-1]:
+        if 'Line' not in str(e.Curve):
             stop
-        e0x = e.Vertexes[0].Point.x
-        e0y = e.Vertexes[0].Point.y
-        e1x = e.Vertexes[1].Point.x
-        e1y = e.Vertexes[1].Point.y
+        first_pnt, common_pnt, last_pnt = find_sequence (edges,i,tol)
         #print(e0x,e0y)
         #print(e1x,e1y)
         #k_edg = "  (gr_line (start {0:.3f} {1:.3f}) (end {2:.3f} {3:.3f}) (angle 0) (layer {5}) (width {4}))"\
         #                .format(x1+ofs[0], y1+ofs[1], x2+ofs[0], y2+ofs[1], edge_width, layer)
         if i < segments_nbr:
-            pts=pts+"         (xy {0:.3f} {1:.3f}) (xy {2:.3f} {3:.3f})".format(e0x+ofs[0], -e0y+ofs[1], e1x+ofs[0], -e1y+ofs[1])+os.linesep
-        else:
-            pts=pts+"         (xy {0:.3f} {1:.3f})".format(e0x+ofs[0], -e0y+ofs[1])+os.linesep+"      )"+os.linesep
+            pts=pts+"         (xy {0:.3f} {1:.3f}) (xy {2:.3f} {3:.3f})".format(first_pnt.x+ofs[0], -first_pnt.y+ofs[1], common_pnt.x+ofs[0], -common_pnt.y+ofs[1])+os.linesep
+        #else:
+        #    pts=pts+"         (xy {0:.3f} {1:.3f})".format(e0x+ofs[0], -e0y+ofs[1])+os.linesep+"      )"+os.linesep
         
         #if i < segments_nbr:
         #    pts=pts+"         (xy "+str(e0x)+" "+str(-1*e0y)+") (xy "+str(e1x)+" "+str(-1*e1y)+")"+os.linesep
         #else:
         #    pts=pts+"         (xy "+str(e0x)+" "+str(-1*e0y)+")"+os.linesep+"      )"+os.linesep
         i=i+1
+    pts=pts+"         (xy {0:.3f} {1:.3f})".format(last_pnt.x+ofs[0], -last_pnt.y+ofs[1])+os.linesep+"      )"+os.linesep
     fillzone += pts
     fillzone+="    )"+os.linesep+"  )"+os.linesep #+")"+os.linesep
     print(fillzone)
