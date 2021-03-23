84d460456cc078d6eefb84837e6152a12b84a943
U-CAD\userC <easyw@katamail.com>
2021-03-23 14:43:24 +0100

    minor update on cheat sheet (dxf export settings)

--- InitGui.py
+++ InitGui.py
@@ -10,11 +10,11 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 10.1.7'
+ksu_wb_version='v 10.1.8'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=418 #v10.1.0
+mycommitsKWB=419 #v10.1.0
 global verKSU
 verKSU="9.7.3.6"
 
