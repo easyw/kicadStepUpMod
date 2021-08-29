31de4fa1040aa577b4792ebdfb7efb3c985c77ac
easyw <easyw@katamail.com>
2021-08-29 18:40:38 +0200

    aligning commit nbrs

--- InitGui.py
+++ InitGui.py
@@ -14,7 +14,7 @@ ksu_wb_version='v 10.2.2'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=427 #v10.1.9
+mycommitsKWB=429 #v10.1.9
 global verKSU
 verKSU="9.7.4.3"
 
