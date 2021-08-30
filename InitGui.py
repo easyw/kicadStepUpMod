87dbfebf1ca1f994ab4334398a096ff9935dc6aa
U-CAD\userC <easyw@katamail.com>
2021-08-30 19:33:11 +0200

    aligning commits

--- InitGui.py
+++ InitGui.py
@@ -14,7 +14,7 @@ ksu_wb_version='v 10.2.2'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=431 #v10.1.9
+mycommitsKWB=434 #v10.1.9
 global verKSU
 verKSU="9.7.5.0"
 
