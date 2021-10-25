1c29085ff4cf4c0c674a17c81a76c86880f5bfcf
U-CAD\userC <easyw@katamail.com>
2021-10-25 18:11:33 +0200

    reverting transparency on step export, adding undo on export 3d model

--- InitGui.py
+++ InitGui.py
@@ -10,13 +10,13 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 10.6.0'
+ksu_wb_version='v 10.6.1'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=467 #v10.6.0
+mycommitsKWB=468 #v10.6.1
 global verKSU
-verKSU="10.1.3.4"
+verKSU="10.1.3.5"
 
 import FreeCAD, FreeCADGui, Part, os, sys
 import re, time
