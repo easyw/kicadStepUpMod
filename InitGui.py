fec319e70542bb3811506dc790aa2924b82c3410
U-CAD\userC <easyw@katamail.com>
2021-10-28 10:16:50 +0200

    improving ksu tuurntable icon

--- InitGui.py
+++ InitGui.py
@@ -10,11 +10,11 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 10.6.5'
+ksu_wb_version='v 10.6.6'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=474 #v10.6.5
+mycommitsKWB=475 #v10.6.6
 global verKSU
 verKSU="10.1.3.8"
 
