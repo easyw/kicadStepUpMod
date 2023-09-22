03f407118dcf134c069e4f0dea6eda2315135aec
U-CAD\userC <easyw@katamail.com>
2023-09-22 18:54:57 +0200

    aligning version number to commits

--- InitGui.py
+++ InitGui.py
@@ -10,13 +10,13 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 10.19.4'
+ksu_wb_version='v 10.19.5'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=603 #  v10.19.4
+mycommitsKWB=605 #  v10.19.5
 global verKSU
-verKSU="10.8.7"
+verKSU="10.8.8"
 
 import FreeCAD, FreeCADGui, Part, os, sys
 import re, time
