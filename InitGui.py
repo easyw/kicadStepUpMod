7fcf80ca19b758e27ba13d25a08f737e5fe68ece
U-CAD\userC <easyw@katamail.com>
2023-12-16 19:24:54 +0100

    fixing a typo

--- InitGui.py
+++ InitGui.py
@@ -10,11 +10,11 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 10.22.3'
+ksu_wb_version='v 10.22.4'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=631 #  v10.22.3
+mycommitsKWB=632 #  v10.22.4
 global verKSU
 verKSU="11.1.4"
 
@@ -414,7 +414,7 @@ class KiCadStepUpWB ( Workbench ):
         ##
         if upd and interval:
             # check_updates(myurlMWB, mycommitsMWB)
-            url=myurlMWB
+            url=myurlKWB
             commit_nbr=mycommitsKWB
             nbr_commits=commits_num.commitCount('easyw','kicadStepUpMod')
             if int(nbr_commits) == 0:
