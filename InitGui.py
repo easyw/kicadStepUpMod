20d1d08a7f6b148e5834fcafbe5dc22abda59559
U-CAD\userC <easyw@katamail.com>
2024-04-21 09:52:49 +0200

    aligning commit num cmtnum=653

--- InitGui.py
+++ InitGui.py
@@ -10,11 +10,11 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 11.01.0'
+ksu_wb_version='v 11.01.2'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=647 #  v11.01.0 NB all the commits must have commit message ending with _cmtnum=nnn
+mycommitsKWB=653 #  v11.01.2 NB all the commits must have commit message ending with _cmtnum=nnn
 # cmtnum=647
 global verKSU
 verKSU="12.0.1"
