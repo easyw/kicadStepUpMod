efa7a69cf73041430585e6f25bd82e64af0f5a49
U-CAD\userC <easyw@katamail.com>
2024-12-04 11:56:59 +0100

    adding gif for tutorial cmtnum=692

--- InitGui.py
+++ InitGui.py
@@ -10,12 +10,12 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 11.04.3'
+ksu_wb_version='v 11.04.4'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=691 #   v11.04.3 NB all the commits must have commit message ending with _cmtnum=nnn
-# cmtnum=691'
+mycommitsKWB=692 #   v11.04.4 NB all the commits must have commit message ending with _cmtnum=nnn
+# cmtnum=692'
 global verKSU
 verKSU="12.3.4"
 
