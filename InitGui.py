e4ce894538c35ca820ef7d7b8f45ee62be88f0c2
U-CAD\userC <easyw@katamail.com>
2026-07-23 18:29:10 +0200

    adding AI policy cmtnum=737

--- InitGui.py
+++ InitGui.py
@@ -10,11 +10,11 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 11.08.9'
+ksu_wb_version='v 11.09.0'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=736 #   NB all the commits must have commit message ending with _cmtnum=nnn
+mycommitsKWB=737 #   NB all the commits must have commit message ending with _cmtnum=nnn
 # cmtnum=735'
 global verKSU
 verKSU="12.8.2"
