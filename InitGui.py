ffbea0430dc832456dcc0aaafbeff16ba9e76452
U-CAD\userC <easyw@katamail.com>
2026-08-09 16:40:42 +0200

    FINALLY aliging commit nums cmtnum=748

--- InitGui.py
+++ InitGui.py
@@ -10,12 +10,12 @@
 #*  Kicad STEPUP (TM) is a TradeMark and cannot be freely usable            *
 #*                                                                          *
 
-ksu_wb_version='v 11.09.3'
+ksu_wb_version='v 11.09.4'
 global myurlKWB, ksuWBpath
 myurlKWB='https://github.com/easyw/kicadStepUpMod'
 global mycommitsKWB
-mycommitsKWB=736 #   NB all the commits must have commit message ending with _cmtnum=nnn
-# cmtnum=736'
+mycommitsKWB=748 #   NB all the commits must have commit message ending with _cmtnum=nnn
+# cmtnum=748'
 global verKSU
 verKSU="13.1.6"
 
